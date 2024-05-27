7f4333222ae1ab00ab10ebbbac5e895b609aa074
Mio1414 <mio14@inbox.ru>
2024-05-27 21:26:04 +0300

    Initial commit

---
+++ src/components/TaskList.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>{{ title }}</h1>
+  {{ list }}
+  <div class="task-list">
+    <ul>
+      <li @click="hello">Задание 1</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'task-list',
+  data() {
+    return{
+      list:[
+        {
+          title:'Помыть посуду'
+        },
+        {
+          title:'Постирать'
+        }
+      ]
+    };
+  },
+  props: {
+    title: String
+  },
+  methods:{
+
+    hello(){
+      alert('Привет');
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
